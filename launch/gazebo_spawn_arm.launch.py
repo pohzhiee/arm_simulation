9ea1541c1dd9b3e3071530e0e40e0d3ed21a87bf
pohzhiee <poh140996@hotmail.com>
2019-10-11 14:26:44 +0800

    update pid params

--- launch/gazebo_spawn_arm.launch.py
+++ launch/gazebo_spawn_arm.launch.py
@@ -13,6 +13,7 @@ def generate_launch_description():
     # Get gazebo_ros package path
     gazebo_ros_share_path = get_package_share_directory('gazebo_ros')
     lobot_desc_share_path = get_package_share_directory('lobot_description')
+    sim_share_path = get_package_share_directory('arm_simulation')
     arm_urdf_path = os.path.join(lobot_desc_share_path,'robots/arm_standalone.urdf')
 
     zero_g_world_path = os.path.join(lobot_desc_share_path, 'worlds/zero_g_world.sdf')
@@ -20,13 +21,17 @@ def generate_launch_description():
     gzserver = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(os.path.join(gazebo_ros_share_path, 'launch',
                     'gzserver.launch.py')),
-        launch_arguments={'extra_gazebo_args': '__log_level:=debug', 'pause': 'true','world': zero_g_world_path}.items())
+        launch_arguments={'extra_gazebo_args': '__log_level:=info', 'pause': 'true','world': zero_g_world_path}.items())
 
     # GAZEBO_MODEL_PATH has to be correctly set for Gazebo to be able to find the model
-    spawn_entity = Node(package='gazebo_ros', node_executable='spawn_entity.py',
-                        arguments=['-entity', 'arm_standalone','-file',arm_urdf_path],
-                        output='screen')
+    #spawn_entity = Node(package='gazebo_ros', node_executable='spawn_entity.py',
+    #                    arguments=['-entity', 'arm_standalone','-file',arm_urdf_path],
+    #                    output='screen')
 
+    spawn_entity = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(os.path.join(sim_share_path, 'launch',
+                    'spawn_entity.launch.py')),
+             )
     # Launch gzclient
     gzclient = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(os.path.join(gazebo_ros_share_path, 'launch',
