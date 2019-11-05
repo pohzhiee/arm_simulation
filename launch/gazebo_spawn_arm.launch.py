4253e14104d0950a4760a3f24b040b961eb863b4
ibrahiminfinite <ibrahimjkd@gmail.com>
2019-11-05 11:15:23 +0530

    minor cleanup

--- launch/gazebo_spawn_arm.launch.py
+++ launch/gazebo_spawn_arm.launch.py
@@ -2,7 +2,7 @@ import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration, PythonExpression
+from launch.substitutions import  LaunchConfiguration, PythonExpression
 from launch.conditions import IfCondition
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
@@ -28,10 +28,6 @@ def generate_launch_description():
                                                    'gzserver.launch.py')),
         launch_arguments={'extra_gazebo_args': '__log_level:=info', 'pause': 'true', 'world': py_cmd}.items())
 
-    # GAZEBO_MODEL_PATH has to be correctly set for Gazebo to be able to find the model
-    # spawn_entity = Node(package='gazebo_ros', node_executable='spawn_entity.py',
-    #                    arguments=['-entity', 'arm_standalone','-file',arm_urdf_path],
-    #                    output='screen')
 
     spawn_entity = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(sim_share_path, 'launch',
